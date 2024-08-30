89be9ee73aed5bd1367a8ccbc2ca48ca4d0c3823
Axel Caldera <axelmoises4@gmail.com>
2024-08-29 21:35:06 -0700

    Ejercicio clase updateButton

--- book-library-frontend/src/components/BookList.js
+++ book-library-frontend/src/components/BookList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function BookList({ books, deleteBook }) {
+function BookList({ books, deleteBook, selectBookToUpdate }) {
   return (
     <ul>
       {books.map((book) => (
         <li key={book.id}>
           {book.title} by {book.author}
           <button onClick={() => deleteBook(book.id)}>Delete</button>
+          <button onClick={() => selectBookToUpdate(book)}>Update Book</button>
         </li>
       ))}
     </ul>
